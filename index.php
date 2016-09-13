434a087580b95732b5ff1a14905f31190074c544
gnsoft <mobileteamdeveloper@gmail.com>
2016-09-13 21:44:55 +0700

    Update944139

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
 echo "tvt";
+/*$mysqli = mysqli_connect(
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    getenv('OPENSHIFT_MYSQL_DB_PORT')
+);*/
+$servername = 'http://mysql-kang.0ec9.hackathon.openshiftapps.com/';
+$username = 'cakephp';
+$password = 'q3xhacvM6G6qSrg3';
+$database = 'default';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    header("HTTP/1.1 503 Service Unavailable");
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "OK";
+echo "tvt2";
