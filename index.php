aacc2ef48f9383a9e40ed9603a0771307b69487e
gnsoft <mobileteamdeveloper@gmail.com>
2016-09-14 19:47:58 +0700

    Update747149

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "tvt:4";
+echo "tvt:5";
 /*$mysqli = mysqli_connect(
     getenv('OPENSHIFT_MYSQL_DB_HOST'), 
     getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
@@ -7,10 +7,10 @@ echo "tvt:4";
     getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
     getenv('OPENSHIFT_MYSQL_DB_PORT')
 );*/
-$servername = 'mysql-ka.0ec9.hackathon.openshiftapps.com';
-$username = 'kang';
-$password = 'kangpedometer';
-$database = 'pedometer';
+$servername = getenv('mysql-ka.0ec9.hackathon.openshiftapps.com');
+$username = getenv('kang');
+$password = getenv('kangpedometer');
+$database = getenv('pedometer');
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
