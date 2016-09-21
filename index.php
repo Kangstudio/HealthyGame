c8d5570da031f26a678317691dbcee9db4c360cc
gnsoft <mobileteamdeveloper@gmail.com>
2016-09-21 18:00:11 +0700

    Update6002109

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($con->connect_error) {
 }*/
 ?>
 <h1>Pedometer fun on Openshift online</h1>
-<p>Adroid application: <a href="http://pedometer-ka.0ec9.hackathon.openshiftapps.com">http://pedometer-ka.0ec9.hackathon.openshiftapps.com/client_application.apk</a></p>
+<p>Adroid application: <a href="http://pedometer-ka.0ec9.hackathon.openshiftapps.com/client_application.apk">http://pedometer-ka.0ec9.hackathon.openshiftapps.com/client_application.apk</a></p>
 <h1>Video:</h1>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/hWa04ERTEI4" frameborder="0" allowfullscreen></iframe>
 </body>
