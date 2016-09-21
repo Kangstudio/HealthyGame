7678f2f2245d363794044653dcd4ec0d40b4aaa7
gnsoft <mobileteamdeveloper@gmail.com>
2016-09-21 17:56:17 +0700

    Update5562109

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Pedometer Fun on OpenShift
+<h1>Pedometer fun on Openshift online</h1>
+<p>Link: <a href="http://pedometer-ka.0ec9.hackathon.openshiftapps.com">http://pedometer-ka.0ec9.hackathon.openshiftapps.com</a></p>
+<h1>Video:</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hWa04ERTEI4" frameborder="0" allowfullscreen></iframe>
--- index.php
+++ index.php
@@ -1,26 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
 <?php
-/*$mysqli = mysqli_connect(
-    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
-    getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
-    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
-    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
-    getenv('OPENSHIFT_MYSQL_DB_PORT')
-);*/
-$servername = getenv('mysql-ka.0ec9.hackathon.openshiftapps.com');
-$username = getenv('kang');
-$password = getenv('kangpedometer');
-$database = getenv('pedometer');
-
 // Create connection
-//$conn = new mysqli($servername, $username, $password, $database);
-$con = new mysqli('172.30.113.88', 'kang', 'kangpedometer', 'pedometer');
+/*$con = new mysqli('172.30.113.88', 'kang', 'kangpedometer', 'pedometer');
 
-// Check connection
 if ($con->connect_error) {
     header("HTTP/1.1 503 Service Unavailable");
     die("Connection failed: " . $con->connect_error);
-}
-
-
-echo "OK - pedometer";
+}*/
 ?>
+<h1>Pedometer fun on Openshift online</h1>
+<p>Adroid application: <a href="http://pedometer-ka.0ec9.hackathon.openshiftapps.com">http://pedometer-ka.0ec9.hackathon.openshiftapps.com/client_application.apk</a></p>
+<h1>Video:</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hWa04ERTEI4" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
